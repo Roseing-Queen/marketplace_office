64bfe83ffd971181591f4f2646a96e52ba9e7a85
常存 <104344457+Roseing-Queen@users.noreply.github.com>
2024-09-29 18:00:49 +0800

    init

--- src/layout/modules/LayoutHeader/index.tsx
+++ src/layout/modules/LayoutHeader/index.tsx
@@ -1,10 +1,36 @@
 import {Header} from "antd/lib/layout/layout";
 import React from "react";
 import './index.scss'
+import {Breadcrumb, Button} from "antd";
+import {HomeOutlined, MenuFoldOutlined} from "@ant-design/icons";
 const LayoutHeader = () => {
     return (
         <Header className={'headerStyle'}>
-            header
+           <div className={"HeaderAll"}>
+               <div className={"HeaderLogo"}>
+                   <Button type="primary" shape="circle" icon={<MenuFoldOutlined />} />
+               </div>
+               <div className={"Breadcrumb"}>
+                   <Breadcrumb
+                       items={[
+                           {
+                               href: '',
+                               title: 'Home',
+                           },
+                           {
+                               href: '',
+                               title: 'Dashboard',
+                           },
+                           {
+                               title: 'Application',
+                           },
+                       ]}
+                   />
+               </div>
+               <div className={"HeaderOption"}>
+                   <HomeOutlined size={24} color={"#0e0e0e"} />
+               </div>
+           </div>
         </Header>
     )
 }
